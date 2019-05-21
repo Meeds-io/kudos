3fee437da29b48ce24069ea6aa67827031d7f70b
Vincent Sellier <vsellier@exoplatform.com>
2019-05-21 16:31:25 +0200

    SWF-4791 prepare the migration to a supported addon

---
+++ kudos-webapps/webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpackCommonConfig = require('./webpack.common.js');
+
+const config = merge(webpackCommonConfig, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "/kudos/fonts/[name].[ext]",
+            emitFile: false
+          }
+        }
+      }
+    ]
+  },
+  entry: {
+    kudos: './src/main/webapp/vue-app/kudos.js',
+    kudosAdmin: './src/main/webapp/vue-app/kudosAdmin.js'
+  },
+  output: {
+    path: path.join(__dirname, 'target/kudos/'),
+    filename: 'js/[name].bundle.js'
+  },
+  externals: {
+    jquery: '$',
+    vuetify: 'Vuetify'
+  }
+});
+
+module.exports = config;
