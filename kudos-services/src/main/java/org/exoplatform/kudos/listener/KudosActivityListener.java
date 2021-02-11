0f9d7c52d74d832301d13966ca61860c560a4f41
GouadriaHanen <hgouadria@exoplatform.com>
2021-02-11 19:33:21 +0100

    updates

--- kudos-services/src/main/java/org/exoplatform/kudos/listener/KudosActivityListener.java
+++ kudos-services/src/main/java/org/exoplatform/kudos/listener/KudosActivityListener.java
@@ -1,23 +1,21 @@
 package org.exoplatform.kudos.listener;
 
-import lombok.SneakyThrows;
-import org.exoplatform.commons.persistence.impl.GenericDAOJPAImpl;
 import org.exoplatform.kudos.dao.KudosDAO;
 import org.exoplatform.kudos.entity.KudosEntity;
-import org.exoplatform.kudos.model.Kudos;
 import org.exoplatform.kudos.service.KudosService;
-import org.exoplatform.kudos.service.KudosStorage;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.social.core.activity.ActivityLifeCycleEvent;
 import org.exoplatform.social.core.activity.ActivityListenerPlugin;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
-
+import org.exoplatform.social.core.processor.I18NActivityProcessor;
 import java.util.Map;
+
+import static org.exoplatform.kudos.service.utils.Utils.KUDOS_ACTIVITY_COMMENT_TYPE;
+import static org.exoplatform.social.core.processor.I18NActivityUtils.getParamValues;
 
 public class KudosActivityListener extends ActivityListenerPlugin {
     private static final Log LOG = ExoLogger.getLogger(KudosActivityListener.class);
-    private static String activityType ="exokudos:activity";
     private KudosDAO kudosDAO;
     private KudosService kudosService;
 
@@ -34,15 +32,17 @@ public class KudosActivityListener extends ActivityListenerPlugin {
     @Override
     public void updateActivity(ActivityLifeCycleEvent activityLifeCycleEvent) {
         ExoSocialActivity activity = (ExoSocialActivity)activityLifeCycleEvent.getSource();
-        String type = activity.getType();
-        if (activity.getType().equals(activityType) ){
+        if (activity.getType().equals(KUDOS_ACTIVITY_COMMENT_TYPE)){
             String msg= activity.getTitle().split(":")[1].split("<")[0];
             Map<String, String> templateParams = activity.getTemplateParams();
-            String RESOURCE_BUNDLE_VALUES_PARAM = templateParams.get("RESOURCE_BUNDLE_VALUES_PARAM") ;
-            String[] toChange = RESOURCE_BUNDLE_VALUES_PARAM.split("#");
+            String  resourve_bundle_values_param= templateParams.get("RESOURCE_BUNDLE_VALUES_PARAM") ;
+            String[] toChange = getParamValues(resourve_bundle_values_param);
             toChange[2]=": " +msg;
-            RESOURCE_BUNDLE_VALUES_PARAM=toChange[0]+'#'+toChange[1]+'#'+toChange[2]+"#"+toChange[3];
-            templateParams.put("RESOURCE_BUNDLE_VALUES_PARAM",RESOURCE_BUNDLE_VALUES_PARAM);
+            StringBuilder new_resourve_bundle_values_param = new StringBuilder().append(toChange[0])
+                    .append("#").append(toChange[1]).append("#")
+                    .append(toChange[2]).append("#")
+                    .append(toChange[3]);
+            templateParams.put("RESOURCE_BUNDLE_VALUES_PARAM",new_resourve_bundle_values_param.toString());
             activity.setTemplateParams(templateParams);
             KudosEntity kudos = kudosService.getKudosByActivityId(Long.parseLong(activity.getId()));
             kudos.setMessage(msg);
