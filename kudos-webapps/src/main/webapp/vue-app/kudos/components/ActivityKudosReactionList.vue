20912b4c0bcb78cd5619083eb5816b828baaa925
nidhalnidhal <nmbarki@exoplatform.com>
2021-11-22 18:11:00 +0100

    TASK-51462 : Kudos tab in reactions drawer when no kudos

--- kudos-webapps/src/main/webapp/vue-app/kudos/components/ActivityKudosReactionList.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/ActivityKudosReactionList.vue
@@ -6,6 +6,16 @@
       :kudos="kudos"
       class="pl-3 pt-2 pb-1" />
   </div>
+  <div v-else class="flex flex-column text-sub-title align-items-center pt-12">
+    <v-icon
+      class="flex d-flex disabled--text pl-1"
+      size="50">
+      fa-award
+    </v-icon>
+    <span class="flex d-flex align-self-center pt-5"> {{ $t('exoplatform.kudos.info.noKudosYet') }} <a class="pl-1" @click="openKudosDrawer"> {{ $t('exoplatform.kudos.info.sendAKudos') }} </a>
+    </span>
+    <span class="flex d-flex align-self-center"> {{ $t('exoplatform.kudos.label.kudosMessagePurpose') }}</span>
+  </div>
 </template>
 <script>
 export default {
@@ -39,6 +49,17 @@ export default {
           console.error('error retrieving activity kudos' , e) ;
         }));
     },
+    openKudosDrawer(event) {
+      if (event) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      document.dispatchEvent(new CustomEvent('exo-kudos-open-send-modal', {detail: {
+        id: this.activityId,
+        parentId: '',
+        type: 'ACTIVITY',
+      }}));
+    }
   },
 };
 </script>
