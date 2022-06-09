d21414fde78c9959ab1c17d7ce5aa89bb84c34c2
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-06-09 09:16:32 +0100

    Task-186: Review the reminder of kudos to send (#177)
    This PR will :
    + review the way we will inform the user by the number of kudos allowed per period and the kudos that the user has already sent.
    + Adding a clickable link to redirect to the send kudos drawer.

--- kudos-webapps/src/main/webapp/vue-app/kudos/initComponents.js
+++ kudos-webapps/src/main/webapp/vue-app/kudos/initComponents.js
@@ -7,6 +7,8 @@ import KudosNotificationsAlert from './components/common/KudosNotificationsAlert
 import ActivityKudosReactionItem from './components/ActivityKudosReactionItem.vue';
 import ActivityKudosReactionList from './components/ActivityKudosReactionList.vue';
 import ActivityKudosReactionEmptyList from './components/common/ActivityKudosReactionEmptyList.vue';
+import KudosOverviewDrawer from './components/common/KudosOverviewDrawer.vue';
+import KudosOverviewItem from './components/common/KudosOverviewItem.vue';
 
 const components = {
   'kudos-api': KudosAPI,
@@ -17,7 +19,9 @@ const components = {
   'kudos-notification-alert': KudosNotificationsAlert,
   'activity-kudos-reaction-item': ActivityKudosReactionItem,
   'activity-kudos-reaction-list': ActivityKudosReactionList,
-  'activity-kudos-reaction-empty-list': ActivityKudosReactionEmptyList
+  'activity-kudos-reaction-empty-list': ActivityKudosReactionEmptyList,
+  'kudos-overview-drawer': KudosOverviewDrawer,
+  'kudos-overview-item': KudosOverviewItem,
 };
 
 for (const key in components) {
