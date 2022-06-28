2b64f141be33331f79c72220421a8787d46b687e
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-06-28 15:53:39 +0100

    MEED-306: Analytics - Kudos sent in a space (#211)
    collecting send kudos in audience Analytics

--- kudos-services/src/main/java/org/exoplatform/kudos/listener/analytics/KudosSentListener.java
+++ kudos-services/src/main/java/org/exoplatform/kudos/listener/analytics/KudosSentListener.java
@@ -113,6 +113,15 @@ public class KudosSentListener extends Listener<KudosService, Kudos> {
         }
       }
     }
+    // kudos sent for a user in a chosen audience
+    if (kudos.getSpacePrettyName() != null) {
+      Space space = getSpaceService().getSpaceByPrettyName(kudos.getSpacePrettyName());
+      Identity spaceIdentity = getIdentity(SpaceIdentityProvider.NAME, kudos.getSpacePrettyName());
+      if (spaceIdentity != null) {
+        streamIdentityId = Long.parseLong(spaceIdentity.getId());
+        addSpaceStatistics(statisticData, space);
+      }
+    }
 
     statisticData.setModule("social");
     statisticData.setSubModule("kudos");
