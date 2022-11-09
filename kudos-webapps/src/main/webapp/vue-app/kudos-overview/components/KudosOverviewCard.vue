a4e4b84b42b1d8281ed1c435815db796e123a3ce
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-11-09 14:04:30 +0100

    feat: Overview Page implement kudos extension registry - MEED-665 - Meeds-io/MIPs#10  (#235)
    * MEED-665 implement kudos extension registry (#230)
    
    this change is going to implement the kudos extension in the my-reputation-overview registry
    
    * fix: kudos icons display - MEED-665 - Meeds-io/MIPs#10 (#231)
    
    this change is going to adpat the kudos display in the 'my reputation' portlet according to the design
    
    * fix: kudos display in my reputation portlet - MEED-665 - Meeds-io/MIPs#10
    
    this change is going to enhance the kudos overview display in the my reputation portlet in overview page .

--- kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row
     :class="clickable && 'clickable' || ''"
-    class="border-box-sizing text-center mx-auto justify-center"
+    class="border-box-sizing text-center justify-center"
     @click="clickable && $emit('open-drawer')">
     <v-col class="my-auto px-0 col-md-auto kudosOverviewIcon">
-      <v-icon class="tertiary-color uiIconAward" size="56" />
+      <v-icon class="tertiary-color uiIconAward" :size="isOverviewDisplay ? '40' : '56'" />
     </v-col>
-    <v-col class="my-auto col-md-auto kudosOverviewCount text-color mx-0 display-2 font-weight-bold">
+    <v-col class="my-auto col-md-auto kudosOverviewCount text-color mx-0 display-1 font-weight-bold">
       <slot name="count"></slot>
     </v-col>
     <v-col class="my-auto col-12 col-md-1 text-md-left px-0 mx-0 kudosOverviewLabel text-color">
@@ -21,6 +21,10 @@ export default {
     clickable: {
       type: Boolean,
       default: null,
+    },
+    isOverviewDisplay: {
+      type: Boolean,
+      default: () => false,
     },
   },
 };
