33321810a7f839c96f2537933679b2291e38254f
ArfaouiMohamedAmine <marfaoui@exoplatform.com>
2022-12-23 17:06:13 +0100

    MEED-1387: enhance kudos display

--- kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
@@ -3,16 +3,16 @@
     :class="clickable && 'clickable' || ''"
     class="border-box-sizing text-center justify-center"
     @click="clickable && $emit('open-drawer')">
-    <v-col class="my-auto px-0 col-md-3 kudosOverviewIcon">
+    <v-col class="my-auto px-0 col-md-3 kudosOverviewIcon pa-0 py-auto">
       <v-icon class="tertiary-color uiIconAward" :size="isOverviewDisplay ? '40' : '56'" />
     </v-col>
-    <v-col class="my-auto col-md-2 kudosOverviewCount text-color mx-0 display-1 font-weight-bold">
+    <v-col class="my-auto col-md-2 kudosOverviewCount text-color mx-0 display-1 font-weight-bold pa-0 py-auto">
       <slot name="count"></slot>
     </v-col>
     <v-col class="my-auto col-12 col-md-1 text-md-left px-0 mx-0 kudosOverviewLabel text-color">
       <v-card
         flat
-        :min-width="isOverviewDisplay ? '55' : '113'">
+        :min-width="isOverviewDisplay ? '55' : '90'">
         <slot name="label"></slot>
       </v-card>
     </v-col>
