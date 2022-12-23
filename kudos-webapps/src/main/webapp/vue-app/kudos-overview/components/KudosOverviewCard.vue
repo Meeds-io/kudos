3a511668a44f86a2f6525d8f4fd014c1237a531d
ArfaouiMohamedAmine <marfaoui@exoplatform.com>
2022-12-23 17:16:26 +0100

    MEED-1387: eliminate enixistant class

--- kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
@@ -3,10 +3,10 @@
     :class="clickable && 'clickable' || ''"
     class="border-box-sizing text-center justify-center"
     @click="clickable && $emit('open-drawer')">
-    <v-col class="my-auto px-0 col-md-3 kudosOverviewIcon pa-0 py-auto">
+    <v-col class="my-auto px-0 col-md-3 kudosOverviewIcon pa-0">
       <v-icon class="tertiary-color uiIconAward" :size="isOverviewDisplay ? '40' : '56'" />
     </v-col>
-    <v-col class="my-auto col-md-2 kudosOverviewCount text-color mx-0 display-1 font-weight-bold pa-0 py-auto">
+    <v-col class="my-auto col-md-2 kudosOverviewCount text-color mx-0 display-1 font-weight-bold pa-0">
       <slot name="count"></slot>
     </v-col>
     <v-col class="my-auto col-12 col-md-1 text-md-left px-0 mx-0 kudosOverviewLabel text-color">
