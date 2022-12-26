3271ec63b576bb548dc6233c50e6abfdb9165f27
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-12-26 17:16:38 +0100

    feat: Adjust Mobile view of kudos when sent/received - MEED-1387 - Meeds-io/MIPs#10 (#262)
    prior to this change, the kudos sent and received are mismatching while screen resolution is changing to revoke this display
    This change is going to to fix this display by fixing the width of labels the behave evenly inorder to achieve this state

--- kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
@@ -3,14 +3,18 @@
     :class="clickable && 'clickable' || ''"
     class="border-box-sizing text-center justify-center"
     @click="clickable && $emit('open-drawer')">
-    <v-col class="my-auto px-0 col-md-3 kudosOverviewIcon">
+    <v-col class="my-auto px-0 col-md-3 kudosOverviewIcon pa-0">
       <v-icon class="tertiary-color uiIconAward" :size="isOverviewDisplay ? '40' : '56'" />
     </v-col>
-    <v-col class="my-auto col-md-2 kudosOverviewCount text-color mx-0 display-1 font-weight-bold">
+    <v-col class="my-auto col-md-2 kudosOverviewCount text-color mx-0 display-1 font-weight-bold pa-0">
       <slot name="count"></slot>
     </v-col>
     <v-col class="my-auto col-12 col-md-1 text-md-left px-0 mx-0 kudosOverviewLabel text-color">
-      <slot name="label"></slot>
+      <v-card
+        flat
+        :min-width="isOverviewDisplay ? '55' : '90'">
+        <slot name="label"></slot>
+      </v-card>
     </v-col>
   </v-row>
 </template>
