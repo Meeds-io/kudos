b0610824c2338c4654a2bb0280fbc7bcdbd7cf8c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-25 16:31:16 +0100

    feat: Replace PLF-ICONS on profile page with fa icons - MEED-1596

--- kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos-overview/components/KudosOverviewCard.vue
@@ -4,7 +4,7 @@
     class="border-box-sizing text-center justify-center align-center d-flex flex-wrap"
     @click="clickable && $emit('open-drawer')">
     <div class="px-0 kudosOverviewIcon pa-0">
-      <v-icon class="tertiary-color uiIconAward" size="40" />
+      <v-icon class="uiIconKudos tertiary-color" size="40">fa fa-award</v-icon>
     </div>
     <div class="kudosOverviewCount text-color mx-2 display-1 font-weight-bold pa-0 d-flex align-center">
       <slot name="count"></slot>
