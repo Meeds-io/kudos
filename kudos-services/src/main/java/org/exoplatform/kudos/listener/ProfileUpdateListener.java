d061790fa659d9aecb1f214eaec65e20eebbbc75
GouadriaHanen <hgouadria@exoplatform.com>
2023-01-26 10:52:49 +0100

    fix: fix user full name and popover after updating user contact in kudos activity - EXO-59990

---
+++ kudos-services/src/main/java/org/exoplatform/kudos/listener/ProfileUpdateListener.java
@@ -0,0 +1,59 @@
+package org.exoplatform.kudos.listener;
+
+import org.exoplatform.kudos.model.Kudos;
+import org.exoplatform.kudos.service.KudosService;
+import org.exoplatform.social.core.profile.ProfileLifeCycleEvent;
+import org.exoplatform.social.core.profile.ProfileListenerPlugin;
+import org.exoplatform.social.core.storage.api.ActivityStorage;
+import org.exoplatform.social.core.storage.cache.CachedActivityStorage;
+
+import java.util.List;
+
+public class ProfileUpdateListener extends ProfileListenerPlugin {
+
+  private ActivityStorage activityStorage;
+
+  private KudosService    kudosService;
+
+  public ProfileUpdateListener(KudosService kudosService, ActivityStorage activityStorage) {
+    this.kudosService = kudosService;
+    this.activityStorage = activityStorage;
+  }
+
+  @Override
+  public void avatarUpdated(ProfileLifeCycleEvent event) {
+    String userId = event.getProfile().getIdentity().getId();
+    this.clearUserActivitiesCache(userId);
+  }
+
+  @Override
+  public void bannerUpdated(ProfileLifeCycleEvent event) {
+
+  }
+
+  @Override
+  public void contactSectionUpdated(ProfileLifeCycleEvent event) {
+    String userId = event.getProfile().getIdentity().getId();
+    this.clearUserActivitiesCache(userId);
+  }
+
+  @Override
+  public void experienceSectionUpdated(ProfileLifeCycleEvent event) {
+
+  }
+
+  @Override
+  public void createProfile(ProfileLifeCycleEvent event) {
+
+  }
+
+  private void clearUserActivitiesCache(String userId) {
+    if (activityStorage instanceof CachedActivityStorage) {
+      long count = kudosService.countKudosByPeriodAndReceiver(Long.parseLong(userId),0 , System.currentTimeMillis());
+      List<Kudos> kudosList = kudosService.getKudosByPeriodAndReceiver(Long.parseLong(userId),0 , System.currentTimeMillis(),(int) count);
+      if (kudosList == null || kudosList.isEmpty())
+        return;
+      kudosList.stream().forEach(kudos -> ((CachedActivityStorage) activityStorage).clearActivityCached(String.valueOf(kudos.getActivityId())));
+    }
+  }
+}
