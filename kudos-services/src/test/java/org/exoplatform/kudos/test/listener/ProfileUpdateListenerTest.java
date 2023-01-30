9fba188723932734a5bcc35597c8070f172730cc
GouadriaHanen <hgouadria@exoplatform.com>
2023-01-30 10:09:46 +0100

    fix: fix kudos test coverage - EXO-59990

--- kudos-services/src/test/java/org/exoplatform/kudos/test/listener/ProfileUpdateListenerTest.java
+++ kudos-services/src/test/java/org/exoplatform/kudos/test/listener/ProfileUpdateListenerTest.java
@@ -2,6 +2,7 @@ package org.exoplatform.kudos.test.listener;
 
 import org.exoplatform.commons.testing.BaseExoTestCase;
 import org.exoplatform.kudos.listener.ProfileUpdateListener;
+import org.exoplatform.kudos.model.Kudos;
 import org.exoplatform.kudos.service.KudosService;
 import org.exoplatform.kudos.test.BaseKudosTest;
 import org.exoplatform.social.core.identity.model.Identity;
@@ -12,6 +13,8 @@ import org.exoplatform.social.core.profile.ProfileLifeCycleEvent;
 import org.exoplatform.social.core.storage.api.ActivityStorage;
 import org.exoplatform.social.core.storage.cache.CachedActivityStorage;
 import org.junit.Test;
+
+import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -31,54 +34,30 @@ public class ProfileUpdateListenerTest extends BaseKudosTest {
     activityStorage = mock(CachedActivityStorage.class);
     Identity rootIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, "root1");
     Profile profile = rootIdentity.getProfile();
-    identityManager.registerProfileListener(new ProfileUpdateListener(kudosService, activityStorage) {
-      @Override
-      public void experienceSectionUpdated(ProfileLifeCycleEvent event) {
-        // noop
-      }
-
-      @Override
-      public void createProfile(ProfileLifeCycleEvent event) {
-        // noop
-      }
-
-      @Override
-      public void technicalUpdated(ProfileLifeCycleEvent event) {
-        // noop
-      }
-
-      @Override
-      public void contactSectionUpdated(ProfileLifeCycleEvent event) {
-        ((CachedActivityStorage) activityStorage).clearActivityCached("1");
-      }
-
-      @Override
-      public void bannerUpdated(ProfileLifeCycleEvent event) {
-        // noop
-      }
-
-      @Override
-      public void avatarUpdated(ProfileLifeCycleEvent event) {
-        ((CachedActivityStorage) activityStorage).clearActivityCached("2");
-      }
-
-      @Override
-      public void aboutMeUpdated(ProfileLifeCycleEvent event) {
-        // noop
-      }
-    });
+    ProfileUpdateListener profileUpdateListener =new ProfileUpdateListener(kudosService, activityStorage);
+    identityManager.registerProfileListener(profileUpdateListener);
+    Kudos kudos = new Kudos() ;
+    kudos.setActivityId(1);
+    when(kudosService.countKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong())).thenReturn(1L);
+    when(kudosService.getKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong(), anyInt())).thenReturn(Collections.singletonList(kudos));
     doNothing().when(((CachedActivityStorage) activityStorage)).clearActivityCached(anyString());
     profile.setProperty(Profile.FIRST_NAME, "Changed Firstname");
     identityManager.updateProfile(profile);
     verify((CachedActivityStorage) activityStorage, times(1)).clearActivityCached(anyString());
+    verify(kudosService, times(1)).countKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong());
+    verify(kudosService, times(1)).getKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong(), anyInt());
 
     profile.setProperty(Profile.ABOUT_ME, "Changed ABOUT_ME");
     profile.removeProperty(Profile.FIRST_NAME);
     identityManager.updateProfile(profile);
     verify((CachedActivityStorage) activityStorage, times(1)).clearActivityCached(anyString());
+    verify(kudosService, times(1)).countKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong());
+    verify(kudosService, times(1)).getKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong(), anyInt());
 
     profile.setProperty(Profile.AVATAR, "new/avatar");
     identityManager.updateProfile(profile);
     verify((CachedActivityStorage) activityStorage, times(2)).clearActivityCached(anyString());
+    verify(kudosService, times(2)).countKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong());
+    verify(kudosService, times(2)).getKudosByPeriodAndReceiver(anyLong(), anyLong(), anyLong(), anyInt());
   }
 }
