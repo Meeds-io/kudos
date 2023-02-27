e5c32581f64671db164bce8165821d2b6c8ddb8f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-02-27 10:23:18 +0100

    feat: Delete useless polyfill JS resources dependency - Meeds-io/MIPs#52 - MEED-1891
    This change will:
    * Delete useless polyfill JS resources dependency
    * Declare Kudos Module as a submodule of Popover JS modules

--- kudos-webapps/src/main/webapp/vue-app/kudos/main.js
+++ kudos-webapps/src/main/webapp/vue-app/kudos/main.js
@@ -29,15 +29,11 @@ const appId = 'KudosApp';
 const lang = (eXo && eXo.env && eXo.env.portal && eXo.env.portal.language) || 'en';
 const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.addon.Kudos-${lang}.json`;
 
-export function init() {
-  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
-  // init Vue app when locale ressources are ready
-    new Vue({
-      template: `<kudos-app id="${appId}" />`,
-      i18n,
-      vuetify,
-    }).$mount(`#${appId}`);
-  });
-}
-
-
+exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+// init Vue app when locale ressources are ready
+  new Vue({
+    template: `<kudos-app id="${appId}" />`,
+    i18n,
+    vuetify,
+  }).$mount(`#${appId}`);
+});
