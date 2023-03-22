41faab0c4a8d144194d1d25c5a1f3d79e7ae1c86
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2023-03-22 15:29:22 +0100

    Fix: Option to delete a kudos message - MEED-1890 - Meeds-io/meeds#781 (#295)
    This PR will allow the space host to delete the kudos activity

--- kudos-services/src/main/java/org/exoplatform/kudos/listener/GamificationIntegrationListener.java
+++ kudos-services/src/main/java/org/exoplatform/kudos/listener/GamificationIntegrationListener.java
@@ -1,6 +1,5 @@
 package org.exoplatform.kudos.listener;
 
-import static org.exoplatform.kudos.service.utils.Utils.ACTIVITY_COMMENT_ID_PREFIX;
 import static org.exoplatform.kudos.service.utils.Utils.GAMIFICATION_GENERIC_EVENT;
 import static org.exoplatform.kudos.service.utils.Utils.KUDOS_ACTIVITY_EVENT;
 import static org.exoplatform.kudos.service.utils.Utils.KUDOS_CANCEL_ACTIVITY_EVENT;
@@ -18,9 +17,7 @@ import org.exoplatform.kudos.service.KudosService;
 import org.exoplatform.services.listener.*;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.social.core.manager.ActivityManager;
-import org.exoplatform.social.core.service.LinkProvider;
 
 /**
  * A listener to add comment or activity
@@ -42,30 +39,16 @@ public class GamificationIntegrationListener extends Listener<KudosService, Kudo
   }
 
   @Override
-  public void onEvent(Event<KudosService, Kudos> event) throws Exception {
+  public void onEvent(Event<KudosService, Kudos> event) {
     ExoContainerContext.setCurrentContainer(container);
     RequestLifeCycle.begin(container);
     try {
-
       Kudos kudos = event.getData();
-      long activityId = kudos.getActivityId();
-      String activityURL = "/";
-      if (activityId > 0) {
-        ExoSocialActivity activity = getActivityManager().getActivity(ACTIVITY_COMMENT_ID_PREFIX + activityId);
-        if (activity != null) {
-          activityURL = LinkProvider.getSingleActivityUrl(activity.getParentId() + "#comment-" + activity.getId());
-        } else {
-          activity = getActivityManager().getActivity(String.valueOf(activityId));
-          if (activity != null) {
-            activityURL = LinkProvider.getSingleActivityUrl(activity.getId());
-          }
-        }
-      }
-
       try {
         Map<String, String> gam = new HashMap<>();
         gam.put("ruleTitle", "sendKudos");
-        gam.put("object", activityURL);
+        gam.put("objectId", String.valueOf(kudos.getActivityId()));
+        gam.put("objectType", "activity");
         gam.put("senderId", kudos.getSenderId()); // matches the gamification's earner id
         gam.put("receiverId", kudos.getReceiverId());
         switch (event.getEventName()) {
@@ -74,8 +57,8 @@ public class GamificationIntegrationListener extends Listener<KudosService, Kudo
           break;
         }
         case KUDOS_CANCEL_ACTIVITY_EVENT: {
-          getActivityManager().deleteActivity(String.valueOf(kudos.getActivityId()));
           listenerService.broadcast(GAMIFICATION_CANCEL_EVENT, gam, String.valueOf(kudos.getTechnicalId()));
+          getActivityManager().deleteActivity(String.valueOf(kudos.getActivityId()));
           break;
         }
         default:
@@ -88,7 +71,8 @@ public class GamificationIntegrationListener extends Listener<KudosService, Kudo
       try {
         Map<String, String> gam = new HashMap<>();
         gam.put("ruleTitle", "receiveKudos");
-        gam.put("object", activityURL);
+        gam.put("objectId", String.valueOf(kudos.getActivityId()));
+        gam.put("objectType", "activity");
         gam.put("senderId", kudos.getReceiverId()); // matches the gamification's earner id
         gam.put("receiverId", kudos.getSenderId());
         switch (event.getEventName()) {
