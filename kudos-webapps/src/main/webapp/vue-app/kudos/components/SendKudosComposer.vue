c00d76cad53d03bdb9ba3812e57dce57d2bcde01
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-08-14 14:32:09 +0100

    feat: Add the ability to send a kudos from the composer - MEED-2071 - meeds-io/meeds#54 (#320)
    This change will add the ability to send a kudos from the composer .

---
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosComposer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-card 
+      class="d-flex flex-row" 
+      @click="openSendKudosDrawer" 
+      outlined 
+      flat 
+      hover>
+      <v-card-actions class="ms-3 me-2">
+        <v-icon
+          color="primary"
+          size="50">
+          fa-award
+        </v-icon>
+      </v-card-actions>
+      <v-card-title class="subtitle-1 font-weight-bold">
+        {{ $t('exoplatform.kudos.title.sendAKudos') }}
+      </v-card-title>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    openSendKudosDrawer() {
+      document.dispatchEvent(new CustomEvent('exo-kudos-open-send-modal', {detail: {
+        id: eXo.env.portal.spaceId,
+        type: 'USER_PROFILE',
+        parentId: '',
+        owner: eXo.env.portal.userName,
+        spaceURL: eXo.env.portal.spaceURL,
+        readOnlySpace: true
+      }}));
+    },
+  },
+};
+</script> 
