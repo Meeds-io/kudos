5ba3d06759da0f9faffd66a8f700e81f8ec9e77b
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-08-15 16:59:47 +0100

    feat: Add the ability to send a kudos from the composer - MEED-2071 - meeds-io/meeds#54  (#323)
    This change will add some adjustments to the composer send kudos button.

--- kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosComposer.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosComposer.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <v-card 
-      class="d-flex flex-row" 
-      @click="openSendKudosDrawer" 
-      outlined 
-      flat 
-      hover>
-      <v-card-actions class="ms-3 me-2">
-        <v-icon
-          color="primary"
-          size="50">
-          fa-award
-        </v-icon>
-      </v-card-actions>
-      <v-card-title class="subtitle-1 font-weight-bold">
-        {{ $t('exoplatform.kudos.title.sendAKudos') }}
-      </v-card-title>
-    </v-card>
-  </div>
+  <v-card 
+    class="d-flex flex-row" 
+    outlined 
+    flat 
+    hover
+    @click="openSendKudosDrawer" >
+    <v-card-actions class="ms-4 py-3">
+      <v-icon
+        color="primary"
+        size="50">
+        fa-award
+      </v-icon>
+    </v-card-actions>
+    <v-card-title class="caption font-weight-bold">
+      {{ $t('exoplatform.kudos.title.sendAKudos') }}
+    </v-card-title>
+  </v-card>
 </template>
 <script>
 export default {
@@ -28,7 +26,7 @@ export default {
         type: 'USER_PROFILE',
         parentId: '',
         owner: eXo.env.portal.userName,
-        spaceURL: eXo.env.portal.spaceURL,
+        spaceURL: eXo.env.portal.spaceUrl,
         readOnlySpace: true
       }}));
     },
