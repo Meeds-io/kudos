699b7ef0f41e85cbe85a063c57cb9ea33ef6ef73
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-09-08 12:07:38 +0100

    feat: Add the ability to send a kudos from the composer - MEED-2071 - Meeds-io/MIPs#54 (#338)
    This change will add the ability to send a kudos from the composer .

--- kudos-webapps/src/main/webapp/vue-app/js/Kudos.js
+++ kudos-webapps/src/main/webapp/vue-app/js/Kudos.js
@@ -219,6 +219,14 @@ export function registerOverviewExtension() {
   }); 
 }
 
+export function registerComposerExtension() {
+  extensionRegistry.registerComponent('ComposerAction', 'composer-action-item', {
+    id: 'sendKudosButton',
+    vueComponent: Vue.options.components['send-kudos-composer'],
+    rank: 1,
+  });
+}
+
 export function registerFavoriteExtensions(title) {
   extensionRegistry.registerExtension('ActivityFavoriteIcon', 'activity-favorite-icon-extensions', {
     id: 'favorite-kudos',
