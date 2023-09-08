a08991626b688aed19a3efa52949dcd204720343
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-09-08 12:52:47 +0100

    feat: Add CTAs below the post CTA - MEED-2072 - Meeds-io/MIPs#54 (#339)
    This fix will change the display of the send kudos action used in the composer.

--- kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosToolbarAction.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosToolbarAction.vue
@@ -1,22 +1,23 @@
 <template>
-  <div
-    class="d-flex flex-row align-center" 
+  <v-btn
+    :ripple="false"
+    class="d-flex flex-row align-center py-2"
+    text
     @click="openSendKudosDrawer">
     <v-icon
       color="primary"
-      size="50">
+      size="27">
       fa-award
     </v-icon>
-    <v-span class="caption font-weight-bold ms-5">
-      {{ $t('exoplatform.kudos.title.sendAKudos') }}
+    <v-span class="body-2 font-weight-bold ms-5  mt-1 dark-grey-color">
+      {{ $t('kudos.title') }}
     </v-span>
-  </div>
+  </v-btn>
 </template>
 <script>
 export default {
   methods: {
     openSendKudosDrawer() {
-      document.dispatchEvent(new CustomEvent('activity-composer-closed'));
       document.dispatchEvent(new CustomEvent('exo-kudos-open-send-modal', {detail: {
         id: eXo.env.portal.userIdentityId,
         type: 'USER_PROFILE',
