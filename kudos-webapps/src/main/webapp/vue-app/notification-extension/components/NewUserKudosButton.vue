7834d4493197b15d39f85f6bbb75a6724bb68106
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-12 10:01:07 +0100

    feat: Add Kudos Button in New User Notification Plugin - MEED-2479 - Meeds-io/MIPs#80

---
+++ kudos-webapps/src/main/webapp/vue-app/notification-extension/components/NewUserKudosButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn
+    id="NewUserKudosButton"
+    class="btn primary px-2"
+    outlined
+    small
+    @click.stop.prevent="openKudosForm">
+    {{ helloIcon }} {{ $t('Notification.plugin.NewUserKudosButtonTitle') }}
+  </v-btn>
+</template>
+<script>
+export default {
+  props: {
+    notification: {
+      type: Object,
+      default: null,
+    },
+  },
+  data: () => ({
+    helloIcon: window.decodeURIComponent('\uD83D\uDC4B'),
+  }),
+  methods: {
+    openKudosForm() {
+      document.dispatchEvent(new CustomEvent('exo-kudos-open-send-modal', {detail: {
+        id: this.notification?.from?.username,
+        type: 'USER_PROFILE',
+      }}));
+    },
+  },
+};
+</script>
