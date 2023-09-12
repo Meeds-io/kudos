d2d8ed1831335776e20b2af608d6f417f514891f
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-09-12 14:56:33 +0100

    feat: Adjust the CTAs display from the general stream - MEED-2073 - Meeds-io/MIPs#54 (#352)
    This change will display the composer send kudos extensions when only the user is in the space stream.

--- kudos-webapps/src/main/webapp/vue-app/js/Kudos.js
+++ kudos-webapps/src/main/webapp/vue-app/js/Kudos.js
@@ -220,16 +220,18 @@ export function registerOverviewExtension() {
 }
 
 export function registerComposerExtension() {
-  extensionRegistry.registerComponent('ComposerAction', 'composer-action-item', {
-    id: 'sendKudosButton',
-    vueComponent: Vue.options.components['send-kudos-composer'],
-    rank: 1,
-  });
-  extensionRegistry.registerComponent('ActivityToolbarAction', 'activity-toolbar-action', {
-    id: 'sendKudosToolbarButton',
-    vueComponent: Vue.options.components['send-kudos-toolbar-action'],
-    rank: 10,
-  });
+  if (eXo.env.portal.spaceGroup?.length) {
+    extensionRegistry.registerComponent('ComposerAction', 'composer-action-item', {
+      id: 'sendKudosButton',
+      vueComponent: Vue.options.components['send-kudos-composer'],
+      rank: 1,
+    });
+    extensionRegistry.registerComponent('ActivityToolbarAction', 'activity-toolbar-action', {
+      id: 'sendKudosToolbarButton',
+      vueComponent: Vue.options.components['send-kudos-toolbar-action'],
+      rank: 10,
+    });
+  }
 }
 
 export function registerFavoriteExtensions(title) {
