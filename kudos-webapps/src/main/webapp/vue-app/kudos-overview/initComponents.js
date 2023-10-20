5e0bd66b4f57ef5cdc57577bab7cb1c02f455b02
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-10-20 14:43:27 +0100

    feat: Add kudos overview row component - MEED-2676 - Meeds-io/MIPs#81 (#403)
    (cherry picked from commit 58553a0db763e099d30cd9ee768badf161c824bc)

--- kudos-webapps/src/main/webapp/vue-app/kudos-overview/initComponents.js
+++ kudos-webapps/src/main/webapp/vue-app/kudos-overview/initComponents.js
@@ -1,9 +1,11 @@
 import KudosOverview from './components/KudosOverview.vue';
 import KudosOverviewCard from './components/KudosOverviewCard.vue';
+import KudosOverviewRow from './components/KudosOverviewRow.vue';
 
 const components = {
   'kudos-overview': KudosOverview,
   'kudos-overview-card': KudosOverviewCard,
+  'kudos-overview-row': KudosOverviewRow,
 };
 
 for (const key in components) {
