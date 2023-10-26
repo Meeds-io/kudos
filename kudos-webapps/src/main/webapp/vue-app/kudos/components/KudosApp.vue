50b94256720ab2a9d9ed79de90389c0379dc66e9
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-10-26 09:25:26 +0100

    fix: Fix the users mention in CKeditor for the kudos case - MEED-2749 - Meeds-io/meeds#1200   (#407)
    This change will fix the mentioned users removed when selecting another audience in the send kudos drawer.

--- kudos-webapps/src/main/webapp/vue-app/kudos/components/KudosApp.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/KudosApp.vue
@@ -9,6 +9,7 @@
 
     <exo-drawer
       ref="activityKudosDrawer"
+      v-model="drawer"
       v-draggable="enabled"
       width="500px"
       hide-actions
@@ -174,8 +175,8 @@
               username-class />
             <div class="d-flex flex-row pt-8">
               <rich-editor
+                v-if="drawer"
                 :ref="ckEditorId"
-                :key="spaceURL"
                 v-model="kudosMessage"
                 :max-length="MESSAGE_MAX_LENGTH"
                 :ck-editor-type="ckEditorType"
@@ -423,7 +424,10 @@ export default {
     },
     displaySenderAvatar() {
       return (this.postInYourNetwork && this.audienceTypesDisplay) || this.isLinkedKudos;
-    }
+    },
+    ckEditorInstance() {
+      return this.drawer && this.$refs.kudosContent || null;
+    },
   },
   methods: {
     init() {
@@ -450,7 +454,7 @@ export default {
         });
     },
     resetEditor() {
-      this.$refs[this.ckEditorId].destroyCKEditor();
+      this.ckEditorInstance.destroyCKEditor();
     },
     initDrawer() {
       this.kudosMessage = '';
@@ -564,7 +568,7 @@ export default {
             this.$refs.activityKudosDrawer.startLoading();
             this.initDrawer()
               .then(() => this.$nextTick())
-              .then(() => this.$refs[this.ckEditorId].initCKEditor())
+              .then(() => this.ckEditorInstance.initCKEditor())
               .finally( () => {
                 this.loading = false;
                 this.$refs.activityKudosDrawer.endLoading();
@@ -582,6 +586,7 @@ export default {
     send() {
       this.error = null;
 
+      const kudosMessage = this.ckEditorInstance.getMessage();
       this.$refs.activityKudosDrawer.startLoading();
       const kudos = {
         entityType: this.entityType,
@@ -589,7 +594,7 @@ export default {
         parentEntityId: this.parentEntityId,
         receiverType: this.receiverType,
         receiverId: this.receiverId,
-        message: this.kudosMessage,
+        message: kudosMessage,
         spacePrettyName: this.audience?.remoteId || this.spaceId
       };
       sendKudos(kudos)
@@ -601,7 +606,7 @@ export default {
           document.dispatchEvent(new CustomEvent('exo-kudos-sent', {detail: kudosSent}));
           return this.$nextTick();
         })
-        .then(() => this.$refs[this.ckEditorId].saveAttachments())
+        .then(() => this.ckEditorInstance.saveAttachments())
         .then(() => {
           return this.init()
             .catch(e => {
