7c02cdc6dedfdb4615266c42f53f6b904677bea1
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-11-22 09:56:52 +0100

    feat: Allow to send Kudos even on owned activity - MEED-2838 - Meeds-io/meeds#1068 (#434)
    This change will allow to send Kudos even on current user owned activity. As usual the user will not be able to send a kudos to himself, in which case, the target user will be prefilled to preserve the usual behavior.

--- kudos-webapps/webpack.watch.js
+++ kudos-webapps/webpack.watch.js
@@ -4,10 +4,11 @@ const { merge } = require('webpack-merge');
 const webpackProductionConfig = require('./webpack.prod.js');
 
 module.exports = merge(webpackProductionConfig, {
-  mode: 'development',
   output: {
     path: '/exo-server/webapps/kudos/',
     filename: 'js/[name].bundle.js',
     libraryTarget: 'amd'
-  }
+  },
+  mode: 'development',
+  devtool: 'eval-source-map'
 });
