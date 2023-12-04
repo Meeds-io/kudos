4224df54cf4273adc7bbe51dce45145edcec09d5
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-12-04 17:05:33 +0100

    feat: Refactor Move Administration to Setup Page - MEED-2896 - Meeds-io/meeds#1271 This change will move Settings into centralized Recognition UI.

--- kudos-webapps/src/main/webapp/vue-app/js/KudosSettings.js
+++ kudos-webapps/src/main/webapp/vue-app/js/KudosSettings.js
@@ -1,5 +1,14 @@
 export function initSettings() {
-  return fetch('/portal/rest/kudos/api/account/settings', {credentials: 'include'})
+  return fetch('/portal/rest/kudos/api/account/settings', {
+    credentials: 'include'
+  })
+    .then(resp => {
+      if (!resp || !resp.ok) {
+        throw new Error('Response code indicates a server error', resp);
+      } else {
+        return resp.json();
+      }
+    })
     .then((resp) => resp && resp.ok && resp.json())
     .then((settings) => (window.kudosSettings = settings ? settings : {}))
     .then(() => getSettings())
@@ -11,21 +20,30 @@ export function initSettings() {
 }
 
 export function getSettings() {
-  return fetch('/portal/rest/kudos/api/settings', {credentials: 'include'}).then((resp) => resp && resp.ok && resp.json());
+  return fetch('/portal/rest/kudos/api/settings', {
+    credentials: 'include',
+  })
+    .then(resp => {
+      if (!resp || !resp.ok) {
+        throw new Error('Response code indicates a server error', resp);
+      } else {
+        return resp.json();
+      }
+    });
 }
 
 export function saveSettings(settings) {
-  if (settings) {
-    return fetch('/portal/rest/kudos/api/settings/save', {
-      credentials: 'include',
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(settings),
-    }).then((resp) => resp && resp.ok);
-  } else {
-    return Promise.resolve(null);
-  }
+  return fetch('/portal/rest/kudos/api/settings/save', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    method: 'POST',
+    body: JSON.stringify(settings),
+  })
+    .then(resp => {
+      if (!resp || !resp.ok) {
+        throw new Error('Response code indicates a server error', resp);
+      }
+    });
 }
