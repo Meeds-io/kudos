37ea5e208194a55bfecc1bf8135babf36ea0725c
Ali HAMDI <ahamdi@exoplatform.com>
2024-01-26 14:51:34 +0100

    fix: reorder user card actions - EXO-68497 (#459)

--- kudos-webapps/src/main/webapp/vue-app/js/Kudos.js
+++ kudos-webapps/src/main/webapp/vue-app/js/Kudos.js
@@ -193,7 +193,7 @@ export function registerExternalExtensions(title) {
     icon: 'fa fa-award uiIconKudos uiIconLightBlue',
     class: 'fas fa-award',
     additionalClass: 'mt-1',
-    order: 20,
+    order: 50,
     enabled: (profile) => profile.enabled && !profile.deleted && profile.username !== eXo.env.portal.userName,
     click: (profile) => {
       const type = profile.prettyName ? 'SPACE_PROFILE' : 'USER_PROFILE';
