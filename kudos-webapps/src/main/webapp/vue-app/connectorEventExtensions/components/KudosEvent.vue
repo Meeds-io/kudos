7c304633b00e249c16c7d22538121b4afa162b2a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-06-28 07:49:10 +0100

    feat: Change default font sizes - MEED-7147 - Meeds-io/MIPs#144

--- kudos-webapps/src/main/webapp/vue-app/connectorEventExtensions/components/KudosEvent.vue
+++ kudos-webapps/src/main/webapp/vue-app/connectorEventExtensions/components/KudosEvent.vue
@@ -18,7 +18,7 @@
 -->
 <template>
   <div v-if="!isEditing">
-    <div class="mb-2">
+    <div class="text-header mb-2">
       {{ $t('gamification.event.display.doItNow') }}
     </div>
     <div class="d-flex justify-center">
