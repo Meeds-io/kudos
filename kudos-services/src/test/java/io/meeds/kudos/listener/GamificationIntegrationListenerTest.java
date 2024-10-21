affeb18953a23b7cc537f8426b6ab076ffc455c9
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-21 15:20:22 +0100

    feat: Replace StringUtils package name - Meeds-io/MIPs#150

--- kudos-services/src/test/java/io/meeds/kudos/listener/GamificationIntegrationListenerTest.java
+++ kudos-services/src/test/java/io/meeds/kudos/listener/GamificationIntegrationListenerTest.java
@@ -30,7 +30,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
