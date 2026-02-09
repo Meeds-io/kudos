64a29997948f2b6fd71bf8af3426703027cc9b3c
MayTekayaa <mtekaya@exoplatform.com>
2026-02-09 09:41:55 +0100

    MEED-10202 Add feedback

--- kudos-webapps/src/main/webapp/vue-app/kudos/components/KudosButton.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/KudosButton.vue
@@ -33,7 +33,7 @@
                   :size="isMobile && '20' || '16'">
                   fa-award
                 </v-icon>
-                <span v-if="!isMobile" class="mx-auto mt-1 mt-lg-0 ms-lg-1 text-body">
+                <span v-if="!isMobile && !$root.reducedWidth" class="mx-auto mt-1 mt-lg-0 ms-lg-1 text-body">
                   {{ $t('exoplatform.kudos.label.kudos') }}
                 </span>
               </div>
