b102a3738ee5f59c35344901625d53d3973426e0
mbensalem <mbensalem@exoplatform.com>
2026-08-27 12:58:30 +0100

    fix: Add missing labels to buttons on the stream page - EXO-89535 (#622)
    This change will add missing labels to buttons on the stream page.

--- kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosToolbarAction.vue
+++ kudos-webapps/src/main/webapp/vue-app/kudos/components/SendKudosToolbarAction.vue
@@ -27,6 +27,7 @@
         icon
         v-bind="attrs"
         v-on="on"
+        :aria-label="$t('exoplatform.kudos.ariaLabel')"
         @click="openSendKudosDrawer">
         <v-icon
           size="20px"
@@ -44,6 +45,7 @@
     id="kudosBtnToolbar"
     class="d-flex flex-row align-center py-2 px-3"
     text
+    :aria-label="$t('exoplatform.kudos.ariaLabel')"
     @click="openSendKudosDrawer">
     <v-icon
       size="27px"
